002006bb0f7f2f0252108c78e62e20aa4f4ace4e
Glitch (hello-webpage) <none>
2021-04-15 17:47:37 +0000

    🚩👩‍👩‍👦 Checkpoint ./ann.js:30733068/390

--- ann.js
+++ ann.js
@@ -3,17 +3,18 @@ var regNumber = "BBIT/MG/0934/05/18";
 
 document.write("<h1>" + personName + "<pre>" + regNumber + "</h1>");
 
-
-
-
-
-
-
-
-var j=1;
-var sum=0;
-while(j<=10){
-sum=sum+j;
-j++;
+var number_one=4;
+var number_two=7;
+if(number_one>number_two){
+  document.write("number one" + "[" + number_one +"]" + "is greater than" + "number two" + "[" + number_two +"]");
+}else{
+  document.write("number two " + "[" + number_two +"]" + " is greater than " +"number one " + "[" +number_one+"]");
 }
-document.write("The sum is" +sum);
+
+
+
+
+
+
+
+
