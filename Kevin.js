447f014f76fa7876fb5f32654259d7f131e0e7a9
Glitch (hello-webpage) <none>
2021-04-15 21:17:52 +0000

    🐮⛳️ Checkpoint ./Kevin.js:30896182/1424 ./index.html:30896182/76

--- Kevin.js
+++ Kevin.js
@@ -2,13 +2,13 @@ var personName = "Kelvin Njenga";
 var regNumber = "BBIT/MG/0908/05/18";
 
 document.write("<h1>" + personName + "<pre>" + regNumber + "</h1>");
-
+//check largest number between 3 numbers
 function largesstnum()
 			{
 				var num1, num2, num3;
 				num1 = Number(document.getElementById("numberfunction").value);
 				num2 = Number(document.getElementById("numbersum").value);
-                num3 = Number(document.getElementById("numbertr").value);
+        num3 = Number(document.getElementById("numbertr").value);
 
 				if(num1>num2 && num1>num3)
 				{
@@ -26,12 +26,17 @@ function largesstnum()
                     document.write("<h2>"+num3+" is larger"+"</h2>");
 				}
 			}
-//if statements to compare two numbers and display which number is large
-var x=4;
-var y=7;
-if(x>y){
-  document.write("The first number " + x + " is greater than" + " the second number:" +y);
-}else{
-  document.write("The second number " + y+ " is greater than " +" the first number" + x);
+//function statement to calculate nnumbers between 1 to 10
+function numberSum(N) {
+  var total = 0;
+    for(var i = 1; i <= N; i++){
+      total += i;
+    }
+    return total;
 }
 
+function run(){
+  var val;
+  val =document.getElementById("val").value;
+  document.getElementById("results").innerHTML=val+": "+numberSum(val)
+  }
--- function.js
+++
@@ -1,8 +0,0 @@
-//if satements to compare two numbers and display which number is large
-var x=4;
-var y=7;
-if(x>y){
-  document.write("The first number " + x + " is greater than" + " the second number:" +y);
-}else{
-  document.write("The second number " + y+ " is greater than " +" the first number" + x);
-}
