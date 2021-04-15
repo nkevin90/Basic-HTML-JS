5168cac8f70e2686c904e364e811d10077d4f796
Glitch (hello-webpage) <none>
2021-04-15 15:07:19 +0000

    🌹🎐 Checkpoint ./index.html:30896182/794 ./kevin.js:30896182/514 ./joe.js:30967933/414

--- joe.js
+++ joe.js
@@ -2,3 +2,11 @@ var personName = "Joseph Mumo";
 var regNumber = "BBIT/MG/1617/05/18";
 
 document.write("<h1>" + personName + "<pre>" + regNumber + "</h1>");
+
+let sum = 0;
+
+for (let i = 1; i <= 1; i++) {
+    sum += i;
+}
+
+console.log('The sum of natural numbers:', sum);
--- kevin.js
+++ kevin.js
@@ -6,3 +6,12 @@ var number1=1;
 var number2=60;
 if(number1>number2)
   document.writeline("<p>"+number2)
+
+var hour = new Date().getHours(); 
+var greeting;
+if (hour < 18) {
+  greeting = "Good day";
+} else {
+  greeting = "Good evening";
+}
+document.getElementById("firstid").innerHTML = greeting;
