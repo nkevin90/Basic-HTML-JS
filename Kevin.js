5c7965e5480fab93b21d077ed144ff9ff655903a
Glitch (hello-webpage) <none>
2021-04-15 22:55:14 +0000

    🍉💿 Checkpoint ./index.html:30896182/274 ./Kevin.js:30896182/702 ./style.css:30896182/32

--- Kevin.js
+++ Kevin.js
@@ -43,3 +43,35 @@ function numberSum() {
     return total;
 }
 
+function run(){
+  let
+  val = document.getElementById("val").value;
+  document.getElementById("results").innerHTML=val+": "+numberSum(val)
+  }
+//display days of the week
+function date(){
+var day;
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case  6:
+    day = "Saturday";
+}
+document.getElementById("demo").innerHTML = "Today is " + day;
+}
