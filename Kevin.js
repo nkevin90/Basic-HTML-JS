96039cf498ecbed68a92309908b9638176636e6c
Glitch (hello-webpage) <none>
2021-04-15 22:04:13 +0000

    🚵🏘 Checkpoint ./index.html:30896182/7905 ./Kevin.js:30896182/8105

--- Kevin.js
+++ Kevin.js
@@ -2,40 +2,42 @@ var personName = "Kelvin Njenga";
 var regNumber = "BBIT/MG/0908/05/18";
 
 document.write("<h1>" + personName + "<pre>" + regNumber + "</h1>");
-//check largest number between 3 numbers
+
 function largesstnum()
 			{
 				var num1, num2, num3;
 				num1 = Number(document.getElementById("numberfunction").value);
 				num2 = Number(document.getElementById("numbersum").value);
-        num3 = Number(document.getElementById("numbertr").value);
+                num3 = Number(document.getElementById("numbertr").value);
 
 				if(num1>num2 && num1>num3)
 				{
 				
-                    document.write("<h2>"+num1+" is larger"+"</h2>");
+                    document.write("<h1>"+num1+" is larger"+"</h1>");
 				}
 				else if(num2>num1 && num2>num3)
 				{
 				
-                    document.write("<h2>"+num2+" is larger"+"</h2>");
+                    document.write("<h1>"+num2+" is larger"+"</h1>");
 				}
 				else(num3>num1 && num3>num2)
 				{
 			
-                    document.write("<h2>"+num3+" is larger"+"</h2>");
+                    document.write("<h1>"+num3+" is larger"+"</h1>");
 				}
 			}
-function load(){
-  var value;
-  value =document.getElementById("val").value;
-  document.getElementById("numbers").innerHTML=value+": "+numberSum(value)
-  }
-
-//function statement to calculate nnumbers between 1 to 10
-function numberSum(n) {
+//if satements to compare two numbers and display which number is larger
+var x=4;
+var y=7;
+if(x>y){
+  document.write("The first number " + x + " is greater than" + " the second number:" +y);
+}else{
+  document.write("The second number " + y+ " is greater than " +" the first number" + x);
+}
+//
+function numberSum(N) {
   var total = 0;
-    for(var i = 1; i <= n; i++){
+    for(var i = 1; i <= N; i++){
       total += i;
     }
     return total;
@@ -43,6 +45,6 @@ function numberSum(n) {
 
 function run(){
   var val;
-  val =document.getElementById("val").value;
+  val = document.getElementById("val").value;
   document.getElementById("results").innerHTML=val+": "+numberSum(val)
   }
