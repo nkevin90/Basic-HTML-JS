b7ce1543f52984def5f26ad2a46962f4a558af98
Glitch (hello-webpage) <none>
2021-04-16 10:19:34 +0000

    🐃🚡 Checkpoint ./Kevin.js:30896182/2067 ./index.html:30896182/7

--- Kevin.js
+++ Kevin.js
@@ -1,43 +1,37 @@
+//This code displays the name of a student and the registration number
 var personName = "Kelvin Njenga";
 var regNumber = "BBIT/MG/0908/05/18";
 
 document.write("<h1>" + personName + "<pre>" + regNumber + "</h1>");
 
-//Checks the largest number between 2 numbers
+//Checks two numbers and displays which number between them is large
 function myFunction() {
   var input;
   var number1 = prompt("enter your 1st number");
   var number2=prompt("enter your 2nd number");
   if (number1>number2) {
-    input = "the first number"+number1+" is larger";
+    document.getElementById("demo").innerHTML="the first number: "+number1+" is larger";
   } else {
-    input = "the second number"+number2+" is larger";
+    document.getElementById("demo").innerHTML="the second number: "+number2+" is larger";
   }
-  document.getElementById("demo").innerHTML="the first number: "+number1+" is larger";
   
 }
 
 //this code display the largest number between three numbers
-function amazing() {
-  var txt;
-  var number1 =prompt("enter your 1st number");
+function amazing(){
+  var input;
+  var number1 = prompt("enter your 1st number");
   var number2=prompt("enter your 2nd number");
-  var number3=prompt("enter your 3rd number")
+  var number3=prompt("enter your 3rd number");
   if (number1>number2 && number1>number3) {
-    txt = "the first number "+number1+" is larger";
-  } else if(number2>number1 && number2>number3) {
-    txt = "the second number "+number2+" is larger";
-    
+    document.getElementById("large").innerHTML="the first number: "+number1+" is larger";
+  } else if(number2>number1 && number2>number3){
+    document.getElementById("large").innerHTML="the second number: "+number2+" is larger";
+  }else {
+    document.getElementById("large").innerHTML="the third number: "+number3+" is larger";
   }
-  else {
-    txt = "the third number "+number3+" is larger";
-    
-  }
-  document.getElementById("large").innerHTML = txt;
 }
-
-//sum of the first 10 natural numbers
-
+  
 
 //display day of the week
 function date(){
