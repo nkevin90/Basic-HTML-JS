47159519c63fc88ace426396722b6f2940dad7c2
Glitch (hello-webpage) <none>
2021-04-16 08:46:57 +0000

    🍠🐯 Checkpoint ./style.css:30896182/48 ./Kevin.js:30896182/311 ./index.html:30896182/65

--- Kevin.js
+++ Kevin.js
@@ -82,3 +82,14 @@ switch (new Date().getDay()) {
 }
 document.getElementById("trial").innerHTML = "Today is " + day;
 }
+//trial
+function myFunction() {
+  var txt;
+  var person = prompt("Please enter your name:", "Harry Potter");
+  if (person == null || person == "") {
+    txt = "User cancelled the prompt.";
+  } else {
+    txt = "Hello " + person + "! How are you today?";
+  }
+  document.getElementById("demo").innerHTML = txt;
+}
